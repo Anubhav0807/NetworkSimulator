5971b654fa4c3dead67f0f32ba84006acc6c6c03
Anubhav Jha <158270421+Anubhav0807@users.noreply.github.com>
2025-01-31 19:20:32 +0530

    Added cables.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var selected;
 var offsetX, offsetY;
+var cables = [];
 
 function handleDragStart(e) {
     this.style.opacity = '0.4';
     selected = e.target;
     offsetX = e.offsetX;
     offsetY = e.offsetY;
-    console.log(`${offsetX} ${offsetY}`);
 }
 
 function handleDragEnter(e) {
@@ -32,6 +32,7 @@ function handleDrop(e) {
     newImg.draggable = false;
     newImg.style.top  = e.clientY - offsetY + 'px';
     newImg.style.left = e.clientX - offsetX + 'px';
+    newImg.addEventListener('click', handleClick);
     this.appendChild(newImg);
 
     selected = null;
@@ -41,6 +42,91 @@ function handleDrop(e) {
 function handleDragEnd(e) {
     this.style.opacity = '1';
     selected = null;
+}
+
+function resizeCanvas() {
+    canvas.width = workspace.offsetWidth;
+    canvas.height = workspace.offsetHeight;
+    cables.forEach((cable) => {
+        drawDiagonal(cable[0], cable[1], cable[2], cable[3]);
+    })
+}
+
+function drawDiagonal(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+}
+
+function isDuplicate(newCable) {
+    let duplicate = false;
+    cables.forEach((cable) => {
+        if (newCable[0] == cable[0] && newCable[1] == cable[1] && newCable[2] == cable[2] && newCable[3] == cable[3]) {
+            duplicate = true;
+            return;
+        }
+        if (newCable[0] == cable[2] && newCable[1] == cable[3] && newCable[2] == cable[0] && newCable[3] == cable[1]) {
+            duplicate = true;
+            return;
+        }
+    });
+    return duplicate;
+}
+
+function handleClick(e) {
+    if (selected) {
+        
+        if (selected != e.target) {
+            let cable = [
+                parseFloat(selected.style.left)+32, 
+                parseFloat(selected.style.top)+32,
+                parseFloat(e.target.style.left)+32,
+                parseFloat(e.target.style.top)+32,
+            ];
+            if (!isDuplicate(cable)) {
+                cables.push(cable);
+            }
+        }
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        cables.forEach((cable) => {
+            drawDiagonal(cable[0], cable[1], cable[2], cable[3]);
+        })
+
+        workspace.removeEventListener('mouseenter', handleMouseEnter);
+        workspace.removeEventListener('mousemove', handleMouseMove);
+        workspace.removeEventListener('mouseleave', handleMouseLeave);
+        selected = null;
+
+        return;
+    }
+    selected = e.target;
+    workspace.addEventListener('mouseenter', handleMouseEnter);
+    workspace.addEventListener('mousemove', handleMouseMove);
+    workspace.addEventListener('mouseleave', handleMouseLeave);
+}
+
+function handleMouseEnter(e) {
+    workspace.addEventListener('mousemove', handleMouseMove);
+}
+
+function handleMouseMove(e) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cables.forEach((cable) => {
+        drawDiagonal(cable[0], cable[1], cable[2], cable[3]);
+    })
+    drawDiagonal(parseFloat(selected.style.left)+32, parseFloat(selected.style.top)+32, e.clientX, e.clientY);
+}
+
+function handleMouseLeave(e) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cables.forEach((cable) => {
+        drawDiagonal(cable[0], cable[1], cable[2], cable[3]);
+    })
+    workspace.removeEventListener('mousemove', handleMouseMove);
 }
 
 let items = document.querySelectorAll('.item');
@@ -54,3 +140,8 @@ workspace.addEventListener('dragenter', handleDragEnter);
 workspace.addEventListener('dragover', handleDragOver);
 workspace.addEventListener('drop', handleDrop);
 workspace.addEventListener('dragleave', handleDragLeave);
+
+canvas = document.querySelector('canvas');
+ctx = canvas.getContext('2d');
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
